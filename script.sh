cf38585cc7e21db6250a7f0cd0aaa8b13b8ae78a
soryxie <1670167385@qq.com>
2025-11-29 03:28:10 +0000

    script

--- script.sh
+++ script.sh
@@ -1,7 +1,11 @@
-python3.13.exe entrypoints/launch.py \
---model_path Wan-AI/Wan2.1-T2V-14B-Diffusers \
---world_size 4 \
---save_disk_path 'output\wan\sp' \
---data_parallel_degree 1 \
---pipefusion_parallel_degree 1 \
---ulysses_parallel_degree 4
+SP=4
+PP=1
+
+python entrypoints/launch.py \
+  --model_path Wan-AI/Wan2.1-T2V-14B-Diffusers \
+  --world_size 4 \
+  --save_disk_path 'output\wan\sp' \
+  --data_parallel_degree 1 \
+  --pipefusion_parallel_degree ${PP} \
+  --ulysses_parallel_degree ${SP} \
+  > "/home/jovyan/shared/yqiao/sparfusionstorage/baseline/xdit/$(date +%Y%m%d_%H%M%S)_SP_${SP}_PP_${PP}.log" 2>&1
